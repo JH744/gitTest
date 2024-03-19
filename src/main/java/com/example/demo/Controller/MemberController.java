1b5db7d9f4c285c18d914b8a8b762b7fad81ac74
JH744 <dydekfflrl@naver.com>
2024-03-19 17:29:03 +0900

    회원 목록 기능 추가함

---
+++ src/main/java/com/example/demo/Controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+	@GetMapping("/listMember")
+	public String listMember() {
+		System.out.println("회원목록 기능 추가함");
+		return "회원목록";
+	}
+	
+}
